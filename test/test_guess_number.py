3f43f02fed7b52e6fde7b3944f0a721798f789f5
Facundo Martín <facu.m077@gmail.com>
2019-05-17 21:56:38 -0300

    unit test for guess_number.py

--- test/test_guess_number.py
+++ test/test_guess_number.py
@@ -5,10 +5,14 @@ from four_number_game import guess_number
 
 class GuessNumberTest(unittest.TestCase):
 
-    # TODO
     def setUp(self):
         self.thinker = guess_number.Thinker()
         self.thinker.number = '5316'
+
+    def test_thinker(self):
+        self.thinker.compare_numbers('1387')
+        self.assertEqual(self.thinker.comparator.good, 1)
+        self.assertEqual(self.thinker.comparator.regular, 1)
 
 
 if __name__ == "__main__":
